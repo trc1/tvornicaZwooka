4b262fbb7897a95e5c68a1255781ce0f9f5673a4
fakofbolan <dario.trcovic@gmail.com>
2024-07-17 14:45:36 +0200

    added music navigation component

---
+++ src/components/music-navigation/MusicNavigation.jsx
@@ -0,0 +1,29 @@
+import ShuffleIcon from "../../assets/icons/ShuffleIcon";
+import PrevIcon from "../../assets/icons/PrevIcon";
+import PlayIcon from "../../assets/icons/PlayIcon";
+import NextIcon from "../../assets/icons/NextIcon";
+import RepeatIcon from "../../assets/icons/RepeatIcon";
+
+export default function MusicNavigation() {
+  return (
+    <nav className="music-navigation">
+      <ol className="music-navigation__wrapper">
+        <li className="music-navigation__icon">
+          <ShuffleIcon />
+        </li>
+        <li className="music-navigation__icon">
+          <PrevIcon />
+        </li>
+        <li className="music-navigation__icon">
+          <PlayIcon />
+        </li>
+        <li className="music-navigation__icon">
+          <NextIcon />
+        </li>{" "}
+        <li className="music-navigation__icon">
+          <RepeatIcon />
+        </li>
+      </ol>
+    </nav>
+  );
+}
