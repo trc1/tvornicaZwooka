a7200bf0030404060dcbdbd2902aaf9e0b3c85c5
fakofbolan <dario.trcovic@gmail.com>
2024-07-18 12:39:20 +0200

    style fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,9 +5,9 @@ import CategoryCard from "../components/category-card/CategoryCard";
 import { feature } from "../data/feature-data";
 import { podcast } from "../data/podcast-data";
 import { featured } from "../data/featured-data";
+import { category } from "../data/category-data";
 
 export default function Dashboard() {
-  const item = featured[0];
   return (
     <div className="content-section">
       <section className="content-section__featured">
@@ -20,14 +20,14 @@ export default function Dashboard() {
           />
         ))}
       </section>
-      <div className="content-section__content">
-        <section className="podcast-wrapper">
-          <header className="podcast-wrapper__header">
-            <h3 className="podcast-wrapper__header-title">Mind Matters</h3>
-            <Button className="btn-outline" label="View All" />
+      <div className="content-section__sections">
+        <section className="section-wrapper">
+          <header className="section-wrapper__header">
+            <h3 className="section-wrapper__header-title">Podcasts</h3>
+            <Button className="btn-small outline" label="View All" />
           </header>
-          <div className="podcast-wrapper__carusel">
-            <div className="podcast-wrapper__carusel-overflow">
+          <div className="podcast__carusel">
+            <div className="podcast__carusel-overflow">
               {podcast.map((item) => (
                 <PodcastCard
                   key={item.title}
@@ -40,8 +40,31 @@ export default function Dashboard() {
             </div>
           </div>
         </section>
+        <section className="section-wrapper">
+          <header className="section-wrapper__header">
+            <h3 className="section-wrapper__header-title">Featured</h3>
+            <Button className="btn-small neutral" label="All" />
+            <Button className="btn-small outline" label="Premium" />
+            <Button className="btn-small outline" label="Disabled" />
+          </header>
+          <div className="section-wrapper__carusel">
+            {featured.map((item) => (
+              <CategoryCard image={item.image} title={item.title} />
+            ))}
+          </div>
+        </section>
+        <section className="section-wrapper">
+          <header className="section-wrapper__header">
+            <h3 className="section-wrapper__header-title">Categories</h3>
+            <Button className="btn-small outline" label="View All" />
+          </header>
+          <div className="section-wrapper__carusel">
+            {category.map((item) => (
+              <CategoryCard image={item.image} title={item.title} />
+            ))}
+          </div>
+        </section>
       </div>
-      <CategoryCard image={item.image} title={item.title} />
     </div>
   );
 }
